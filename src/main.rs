b8726d2e2253c57509e7eff6b3e8dbb95465a181
Underdisc <connortdeakin@gmail.com>
2025-01-24 21:50:05 +0000

    basic movement and environment setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Player;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(FixedUpdate, player_movement)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(0.5, 0.5, 5.0))),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+        Transform::from_xyz(-2.0, 0.0, 0.0),
+    ));
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(0.5, 0.5, 5.0))),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+        Transform::from_xyz(2.0, 0.0, 0.0),
+    ));
+
+    let camera_direction: Vec3 = Vec3::normalize(Vec3::new(0.0, -1.0, 1.0));
+    commands
+        .spawn((
+            Mesh3d(meshes.add(Sphere::new(0.1))),
+            MeshMaterial3d(materials.add(Color::BLACK)),
+            Transform::from_xyz(0.0, 0.0, 0.0),
+            Player,
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                Camera3d::default(),
+                Transform::from_xyz(0.0, 3.0, 3.0).looking_at(camera_direction, Vec3::Y),
+            ));
+        });
+}
+
+fn player_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut player_transform: Single<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    let mut movement: Vec2 = Vec2::new(0.0, 0.0);
+    if keyboard_input.pressed(KeyCode::KeyE) {
+        movement += Vec2::new(0.0, 1.0);
+    }
+    if keyboard_input.pressed(KeyCode::KeyD) {
+        movement += Vec2::new(0.0, -1.0);
+    }
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        movement += Vec2::new(-1.0, 0.0);
+    }
+    if keyboard_input.pressed(KeyCode::KeyF) {
+        movement += Vec2::new(1.0, 0.0);
+    }
+    if Vec2::length_squared(movement) > 0.0 {
+        let speed = 1.0;
+        movement = time.delta_secs() * speed * Vec2::normalize(movement);
+        player_transform.translation.x += movement.x;
+        player_transform.translation.z += movement.y;
+    }
 }
